357375806aa6a9ca8993330cc8e2e768ac761375
okasho <okasho1010@gmail.com>
2016-02-23 13:22:27 +0900

    create file accesses

---
+++ iOS/kyutech/Direction.swift
@@ -0,0 +1,9 @@
+//
+//  Direction.swift
+//  kyutech
+//
+//  Created by shogo okamuro on 2/23/16.
+//  Copyright © 2016 shogo okamuro. All rights reserved.
+//
+
+import Foundation
---
+++ iOS/kyutech/TimeTable.swift
@@ -0,0 +1,9 @@
+//
+//  TimeTable.swift
+//  kyutech
+//
+//  Created by shogo okamuro on 2/23/16.
+//  Copyright © 2016 shogo okamuro. All rights reserved.
+//
+
+import Foundation
