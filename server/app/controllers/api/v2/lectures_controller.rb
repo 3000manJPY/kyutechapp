6e633d0cefe51717a4dc29016ad3b58d30045fb5
okasho <okasho@planningdev.com>
2016-02-29 22:20:40 +0900

    lectureController でサーバータイムを追加した

--- server/app/controllers/api/v2/lectures_controller.rb
+++ server/app/controllers/api/v2/lectures_controller.rb
@@ -8,7 +8,8 @@ class Api::V2::LecturesController < ApplicationController
     else
       @lectures = Lecture.where("campus_id = ?", @campus_id)
     end
-    render json: @lectures
+    @SYLLABUS_UPDATE_TIME = "com.planningdev.kyutech.lecture.update"
+    render json: { "data" => @lectures, "server_time" => (Rails.cache.read @SYLLABUS_UPDATE_TIME)}
   end
 
   def lecture
