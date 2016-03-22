feb14a11cb3e88e6d374e7f4f803fe3af9291581
okasho <okasho@planningdev.com>
2016-03-22 11:52:20 +0900

    delete add_access_id_to_access_campus.rb

--- server/db/migrate/20160321235950_add_access_id_to_access_campus.rb
+++
@@ -1,6 +0,0 @@
-class AddAccessIdToAccessCampus < ActiveRecord::Migration
-  def change
-    add_column :access_campuses, :access_id, :integer
-    add_column :access_campuses, :campus_id, :integer
-  end
-end
